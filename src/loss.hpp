b661066b69afe9b486d3f109230b31895fd2e58a
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2020-03-03 01:11:21 +0500

    Add loss interface

---
+++ src/loss.hpp
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <armadillo>
+
+class ILoss {
+public:
+    [[nodiscard]] virtual double GetLoss(const arma::mat &inputs, const arma::mat &outputs) const = 0;
+
+    [[nodiscard]] virtual arma::mat GetGradients(const arma::mat &input, const arma::mat &outputs) const = 0;
+
+    virtual ~ILoss() = default;
+};
+
+class MSELoss : public ILoss {
+public:
+    [[nodiscard]] double GetLoss(const arma::mat &inputs, const arma::mat &outputs) const override {
+        return arma::accu(arma::pow(inputs - outputs, 2)) / inputs.n_rows;
+    }
+
+    [[nodiscard]] arma::mat GetGradients(const arma::mat &inputs, const arma::mat &outputs) const override {
+        return 2 * (inputs - outputs) / inputs.n_rows;
+    }
+};
