82a57f23bcb5affbe9e52b2ae8d3703cb59eab66
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2020-03-03 01:11:33 +0500

    Add loss in neural_network

--- src/neural_network.hpp
+++ src/neural_network.hpp
@@ -2,17 +2,21 @@
 
 #include "layers.hpp"
 #include "optimizer.hpp"
+#include "loss.hpp"
 #include <memory>
 
 class NeuralNetwork {
 public:
-    explicit NeuralNetwork(std::unique_ptr<IOptimizer> _optimizer) : layers(), optimizer(std::move(_optimizer)) {}
+    explicit NeuralNetwork(std::unique_ptr<IOptimizer> _optimizer, std::unique_ptr<ILoss> _loss)
+            : layers(), optimizer(std::move(_optimizer)), loss(std::move(_loss)) {
+
+    }
 
     void AddLayer(std::unique_ptr<ILayer> layer) {
         layers.emplace_back(std::move(layer));
     }
 
-    ILayer *GetLayer(int layer_id) const {
+    [[nodiscard]] ILayer *GetLayer(int layer_id) const {
         return layers[layer_id].get();
     }
 
@@ -25,7 +29,7 @@ public:
         return output.str();
     }
 
-    void Fit(const arma::mat &input, const arma::mat &output) {
+    double Fit(const arma::mat &input, const arma::mat &output) {
         DLOG(INFO) << "Fitting neural network...";
         std::vector<arma::mat> inter_outputs = {input};
         for (auto &&layer : layers) {
@@ -33,20 +37,22 @@ public:
             inter_outputs.push_back(layer->Apply(inter_outputs.back()));
         }
         std::vector<std::vector<arma::mat>> layer_gradients = {};
-        // todo: specify cost function
-        std::vector<arma::mat> output_gradients = {2 * (inter_outputs.back() - output) / input.n_rows};
+        DLOG(INFO) << "Expected outputs: " << std::endl << output << std::endl
+                   << "Actual outputs: " << std::endl << inter_outputs.back();
+        std::vector<arma::mat> output_gradients = {loss->GetGradients(inter_outputs.back(), output)};
         for (int i = static_cast<int>(layers.size()) - 1; i >= 0; i--) {
             DLOG(INFO) << "Propagate gradients backward for layer: " << layers[i]->GetName();
-            DLOG(INFO) << "Intermediate output: " << inter_outputs[i] << std::endl
-                       << "Output gradients: " << output_gradients.back();
+            DLOG(INFO) << "Intermediate output: " << std::endl << inter_outputs[i] << std::endl
+                       << "Output gradients: " << std::endl << output_gradients.back();
             auto gradients = layers[i]->PullGradientsBackward(inter_outputs[i], output_gradients.back());
             auto gradients_to_apply = optimizer->GetGradientStep(gradients.layer_gradients);
-            DLOG(INFO) << "Gradients for layer: " << layers[i]->GetName() << " - " << std::endl
+            DLOG(INFO) << "Gradients for layer: " << layers[i]->GetName() << std::endl
                        << gradients.layer_gradients;
             layers[i]->ApplyGradients(gradients_to_apply);
             output_gradients.push_back(gradients.input_gradients);
         }
         DLOG(INFO) << "Fitting finished";
+        return loss->GetLoss(inter_outputs.back(), output);
     }
 
     [[nodiscard]] arma::mat Predict(const arma::mat &input) const {
@@ -63,4 +69,5 @@ public:
 private:
     std::vector<std::unique_ptr<ILayer>> layers;
     std::unique_ptr<IOptimizer> optimizer;
+    std::unique_ptr<ILoss> loss;
 };
