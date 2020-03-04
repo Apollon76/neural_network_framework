a48821ebeb5e7da4790a09f05ec2bcae1370d192
Sivukhin Nikita <sivukhin.nikita@yandex.ru>
2020-03-04 23:32:48 +0500

    Fill one-hot matrix with zeros

--- src/data_processing/data_utils.hpp
+++ src/data_processing/data_utils.hpp
@@ -7,7 +7,7 @@ namespace nn_framework::data_processing {
     arma::s32_mat OneHotEncoding(const arma::s32_mat& matrix) {
         ensure(matrix.n_cols == 1, "one hot available only for Nx1 matrices");
         auto max = matrix.max();
-        arma::s32_mat result(matrix.n_rows, max + 1);
+        arma::s32_mat result(matrix.n_rows, max + 1, arma::fill::zeros);
         for (size_t i = 0; i < matrix.n_rows; i++) {
             result.row(i).col(matrix.at(i, 0)) = 1;
         }
