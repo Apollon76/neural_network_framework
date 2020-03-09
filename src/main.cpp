771d25ba77f288e283b821a61163ffe774d4ea24
Shibaev Aleksandr Ivanovich <a.shibaev@stash.tcsbank.ru>
2020-03-09 21:45:57 +0500

    Add adam optimizer

--- src/main.cpp
+++ src/main.cpp
@@ -64,8 +64,8 @@ void Sample() {
     std::cout << neural_network.ToString() << std::endl;
 }
 
-NeuralNetwork BuildMnistNN() {
-    auto neural_network = NeuralNetwork(std::make_unique<Optimizer>(0.00001),
+NeuralNetwork BuildMnistNN(std::unique_ptr<IOptimizer> optimizer) {
+    auto neural_network = NeuralNetwork(std::move(optimizer),
                                         std::make_unique<CategoricalCrossEntropyLoss>());
     neural_network.AddLayer(std::make_unique<DenseLayer>(784, 100))
             .AddLayer(std::make_unique<SigmoidActivationLayer>())
@@ -98,21 +98,21 @@ void FitNN(NeuralNetwork *neural_network,
     }
 }
 
-void DigitRecognizer(const std::string& data_path) {
+void DigitRecognizer(const std::string& data_path, const std::string& output, std::unique_ptr<IOptimizer> optimizer) {
     auto [x_train, y_train] = LoadMnist(data_path + "/data/kaggle-digit-recognizer/train.csv");
     auto x_test = LoadMnistX(data_path + "/data/kaggle-digit-recognizer/test.csv");
 
     std::cout << "X: " << FormatDimensions(x_train) << " y: " << FormatDimensions(y_train) << std::endl;
     LOG(INFO) << "Start digit-recognizer neural network...";
 
-    auto neural_network = BuildMnistNN();
-    FitNN(&neural_network, 20, x_train, y_train);
+    auto neural_network = BuildMnistNN(std::move(optimizer));
+    FitNN(&neural_network, 40, x_train, y_train);
 
     auto train_score = nn_framework::scoring::one_hot_accuracy_score(neural_network.Predict(x_train), y_train);
     std::cout << "Final train score: " << train_score << std::endl;
 
     arma::ucolvec predictions = arma::index_max(neural_network.Predict(x_test), 1);
-    std::string predictions_path = data_path + "/data/kaggle-digit-recognizer/predictions.csv";
+    std::string predictions_path = data_path + "/data/kaggle-digit-recognizer/" + output;
     nn_framework::io::CsvWriter writer(predictions_path);
     writer.WriteRow({"ImageId", "Label"});
     for (arma::u64 i = 0; i < predictions.n_rows; i++) {
@@ -128,7 +128,7 @@ void Mnist(const std::string& data_path) {
     std::cout << "X: " << FormatDimensions(x_train) << " y: " << FormatDimensions(y_train) << std::endl;
 
     LOG(INFO) << "Start mnist neural network...";
-    auto neural_network = BuildMnistNN();
+    auto neural_network = BuildMnistNN(std::make_unique<Optimizer>(0.00001));
     FitNN(&neural_network, 20, x_train, y_train, x_test, y_test);
 
     auto train_score = nn_framework::scoring::one_hot_accuracy_score(neural_network.Predict(x_train), y_train);
@@ -148,7 +148,12 @@ int main(int argc, char **argv) {
     auto data_path = parsed_args["data"].as<std::string>();
 
     // Mnist(data_path);
-    DigitRecognizer(data_path);
+//    DigitRecognizer(data_path, "predictions-sgd-0.001.csv", std::make_unique<Optimizer>(0.0001));
+//    DigitRecognizer(data_path, "predictions-momentum-0.01.csv", std::make_unique<MomentumOptimizer>(0.0001, 0.0001));
+    DigitRecognizer(data_path, "predictions-rmsprop-0.01-40.csv", std::make_unique<RMSPropOptimizer>(0.01));
+//    DigitRecognizer(data_path, "predictions-adam-0.01.csv", std::make_unique<AdamOptimizer>(0.01));
+//    DigitRecognizer(data_path, "predictions-adam-0.001.csv", std::make_unique<AdamOptimizer>(0.001));
+//    DigitRecognizer(data_path, "predictions-adam-0.0001.csv", std::make_unique<AdamOptimizer>(0.0001));
     // Sample();
     return 0;
 }
