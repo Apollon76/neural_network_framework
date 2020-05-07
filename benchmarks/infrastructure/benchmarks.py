f61d18152c04984cb3f5c5bc73b7d4cd12c5b531
mpivko <mpivko@yandex.ru>
2020-05-07 20:26:06 +0500

    Fix

--- benchmarks/infrastructure/benchmarks.py
+++ benchmarks/infrastructure/benchmarks.py
@@ -158,10 +158,10 @@ def main():
         print('Script should be called in ./benchmarks directory')
         exit(1)
 
-    tests = {
+    tests = [
         ('mnist', 32, 10),
         ('cifar', 128, 5),
-    }
+    ]
     for test_name, batch_size, epochs in tests:
         data_path = f'cases/{test_name}/data'
 
